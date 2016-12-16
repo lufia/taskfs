c4d6785471bd10712ce08e8d755c019495f4f62a
kadota <kadota@fenrir.co.jp>
2016-12-16 20:29:24 +0900

    gitlab: implement comments

--- gitlab/gitlab.go
+++ gitlab/gitlab.go
@@ -8,6 +8,27 @@ import (
 	"github.com/xanzy/go-gitlab"
 )
 
+type Comment struct {
+	num  int
+	note *gitlab.Note
+}
+
+func (p *Comment) Key() string {
+	return fmt.Sprintf("%d", p.num)
+}
+
+func (p *Comment) Message() string {
+	return p.note.Body
+}
+
+func (p *Comment) Creation() time.Time {
+	return *p.note.CreatedAt
+}
+
+func (p *Comment) LastMod() time.Time {
+	return *p.note.UpdatedAt
+}
+
 type Issue struct {
 	issue *gitlab.Issue
 	svc   *Service
@@ -41,8 +62,36 @@ func (p *Issue) LastMod() time.Time {
 }
 
 func (p *Issue) Comments() (a []fs.Comment, err error) {
-	// TODO: implement
-	return
+	var buf []*gitlab.Note
+	page := 0
+	for {
+		var b []*gitlab.Note
+		b, page, err = p.fetchNotes(page)
+		if err != nil {
+			return
+		}
+		buf = append(buf, b...)
+		if page == 0 {
+			break
+		}
+	}
+	a = make([]fs.Comment, len(buf))
+	for i, v := range buf {
+		a[i] = &Comment{num: i + 1, note: v}
+	}
+	return a, nil
+}
+
+func (p *Issue) fetchNotes(page int) ([]*gitlab.Note, int, error) {
+	pid := p.issue.ProjectID
+	n := p.issue.ID
+	var opt gitlab.ListIssueNotesOptions
+	opt.Page = page
+	b, resp, err := p.svc.c.Notes.ListIssueNotes(pid, n, &opt)
+	if err != nil {
+		return nil, 0, err
+	}
+	return b, resp.NextPage, nil
 }
 
 type Config struct {
